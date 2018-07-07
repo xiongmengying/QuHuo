87db30032f0bb1398fda4e046ceab5cbde65c5ae
SH\sh-qinzy <13311763396@163.com>
2018-07-07 12:55:42 +0800

    Revert "update"
    This reverts commit 06bb31168f0f8a44ea8968754ca05546e1bfd6b6.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="Intro" style="background: url('static/images/back/contact.jpg') no-repeat center center;background-size: cover;">
     <div class="content">
-      <ul>
-        <li style="background: url('static/images/back/contact-1.jpg') no-repeat center center;background-size: cover;">
-          <p>上海</p>
-          <p>邮箱：william.zhang@lfalways.com</p>
-        </li>
-        <li style="background: url('static/images/back/contact-2.jpg') no-repeat center center;background-size: cover;">
-          <p>深圳</p>
-          <p>邮箱：hannah.han@lfalways.com</p>
-        </li>
-        <li style="background: url('static/images/back/contact-3.jpg') no-repeat center center;background-size: cover;">
-          <p>北京</p>
-          <p>邮箱：dana.xie@lfalways.com</p>
-        </li>
-      </ul>
+      
     </div>
   </div>
 </template>
@@ -27,28 +14,8 @@
   .content{
     width: auto;
     z-index: 99;
-    padding-top: 150px;
     text-align: center;
     margin: 0 auto;
-    ul{
-      width: 100%;
-      height: 420px;
-      li{
-        display: inline-block;
-        width: 600px;
-        height: 420px;
-        padding-top: 150px;
-        background-color: red;
-        margin-right: 10px;
-        p{
-          line-height: 28px;
-          font-size: 28px;
-          color: #ffffff;
-          font-weight: bold;
-          margin-bottom: 20px;
-        }
-      }
-    }
   }
 }
 </style>
