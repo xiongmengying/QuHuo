7b190cf2cf2ac9040c79c99a77cf32e67dc5d299
SH\sh-qinzy <13311763396@163.com>
2018-07-08 14:06:15 +0800

    vvv

--- src/components/Quhuo.vue
+++ src/components/Quhuo.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="swiper-container" id="swiper">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide" v-for="(item, index) in detail" :key="index">
-        <a v-on:click="showPreview(index,detail,swiperObj)">
-          <figure class="wp-avatar banner">
-            <img v-cloak :src="item"/>
-          </figure>
-        </a>
-      </div>
-    </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
-  </div>
+  
 </template>
 
 <script>
@@ -23,21 +11,10 @@ export default {
   data() {
     return {
       detail: [
-        'static/images/clients/IT/intel.png',
-        'static/images/clients/IT/dareu.png',
-        'static/images/clients/IT/hp.png'
       ]
     }
   },
   mounted() {
-    setTimeout(function () {
-      state.swiperObj =  new Swiper('#swiper', {
-        loop: true,
-        pagination: '.swiper-pagination',
-        autoplay: 2000,
-        paginationClickable: true
-      });
-    }, 100)
   }
 }
 </script>
