db2ecd6d1baf0c9e7fdc03873eb2d91798d25879
SH\sh-qinzy <13311763396@163.com>
2018-07-08 14:04:24 +0800

    aaa

---
+++ src/components/Quhuo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="swiper-container" id="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(item, index) in detail" :key="index">
+        <a v-on:click="showPreview(index,detail,swiperObj)">
+          <figure class="wp-avatar banner">
+            <img v-cloak :src="item"/>
+          </figure>
+        </a>
+      </div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.min.css';
+
+export default {
+  name: 'quhuo',
+  data() {
+    return {
+      detail: [
+        'static/images/clients/IT/intel.png',
+        'static/images/clients/IT/dareu.png',
+        'static/images/clients/IT/hp.png'
+      ]
+    }
+  },
+  mounted() {
+    setTimeout(function () {
+      state.swiperObj =  new Swiper('#swiper', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        autoplay: 2000,
+        paginationClickable: true
+      });
+    }, 100)
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { Carousel, CarouselItem } from 'element-ui';
+import Intro from '@/components/Intro'
+import About from '@/components/About'
+import Architecture from '@/components/Architecture'
+import Resource from '@/components/Resource'
+import Business from '@/components/Business'
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.min.css';
 
 Vue.config.productionTip = false
 
 Vue.use(Carousel)
 Vue.use(CarouselItem)
+Vue.component('Intro', Intro)
+Vue.component('About', About)
+Vue.component('Architecture', Architecture)
+Vue.component('Resource', Resource)
+Vue.component('Business', Business)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
